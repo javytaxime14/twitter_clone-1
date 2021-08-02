582d15161770853b55230e290db985027a422c71
Javiera Hidalgo <javiera_56@hotmail.com>
2021-08-01 23:36:45 -0400

    last design updates, added user profile views

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations'}
   get 'home/index'
   root to: 'home#index'
-  get "/app/assets/images/orange-star.png", to: redirect('/'), constraints: lambda { |req|
-  req.path.exclude? 'rails/active_storage'
-}
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
