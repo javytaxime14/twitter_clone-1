97208fcbef035e57065b05cbdc956ce13d76c976
Javiera Hidalgo <javiera_56@hotmail.com>
2021-08-03 03:16:44 -0400

    correcting errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       # GET /users/1.json
       def show
         @user = User.find(params[:id])
-        if user_signed_in? && current_user != @user 
+        if user_signed_in? && current_user.id != @user.id 
           if current_user.is_following?(params[:friend_id])
             Friend.where(user_id: current_user.id, friend_id: params[:friend_id]).delete_all
           else
